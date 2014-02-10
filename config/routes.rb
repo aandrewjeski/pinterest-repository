1e90c0eee0eb230f2e6461190bdbf92d305d13f5
Adam Andrewjeski <aandrewjeski@gmail.com>
2014-02-09 17:24:57 -0800

    Customize Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Pinterest::Application.routes.draw do
   root "pages#home"
-  get "about" => "pages#about"
+  get "about" => "pages#about" # creats about_path
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
