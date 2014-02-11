53ceaad22dd56873b18a45822aec7522af210946
Adam Andrewjeski <aandrewjeski@gmail.com>
2014-02-10 19:52:07 -0800

    added user signup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Pinterest::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #required for heroku
+   config.action_mailer.default_url_options = { :host => 'http://omr-pinterest-project.herokuapp.com/' }
 end
